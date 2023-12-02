914daf8bcf6e9f11a3669e3f8a477de49633ec6e
Neaz Mahmood <neazn694@gmail.com>
2023-12-03 00:48:15 +0600

    added contact us page

---
+++ src/Pages/Contact/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Contact.css";
+import ContactForm from '../ContactForm/ContactForm';
+
+const Contact = () => {
+  return (
+    <ContactForm></ContactForm>
+  )
+}
+
+export default Contact;
